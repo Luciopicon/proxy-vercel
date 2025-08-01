a1b78c49eafc8027cc3c8344fd19694215e81a1d
Luciopicon <100102765+Luciopicon@users.noreply.github.com>
2025-08-01 14:43:35 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,25 +1,11 @@
-import { createServer } from 'http';
-import { WebSocketServer } from 'ws';
-import { createProxyServer } from 'http-proxy';
-
-const server = createServer();
-const proxy = createProxyServer({ target: 'ws://185.194.204.52:8080', ws: true });
-
-// WebSocket handler
-const wss = new WebSocketServer({ noServer: true });
-wss.on('connection', socket => {
-  console.log('WebSocket conectado');
-  socket.on('message', msg => {
-    console.log('Recebido:', msg.toString());
-  });
-});
-
-// Upgrade handler
-server.on('upgrade', (req, socket, head) => {
-  proxy.ws(req, socket, head);
-});
-
-// Start
-server.listen(8080, () => {
-  console.log('Proxy WebSocket ativo na porta 8080');
-});
+// next.config.js
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/:path*", // Captura todas as rotas
+        destination: "https://185.194.204.52/:path*", // Seu servidor real
+      },
+    ];
+  },
+};
